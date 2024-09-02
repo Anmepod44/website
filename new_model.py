c47770c43b33bef46ef67269b815f5c30aa4ece8
MARVIN <mnyaranga44@gmail.com>
2024-09-02 17:18:36 +0300

    initial-commit

--- new_model.py
+++ new_model.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-HOST='3.13.33.220'
+
 # SQLAlchemy setup for MySQL
-engine = create_engine(f'mysql+pymysql://root:example@mysql:3306/')
+engine = create_engine(f'mysql+pymysql://root:example@mysql:3306/ZAHLENTECH')
 Session = sessionmaker(bind=engine)
 session = Session()
 
